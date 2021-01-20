5bb927a0fb2cbc83a382afe147d2f08e3d54d1d2
tio-plato <60922756+tio-plato@users.noreply.github.com>
2021-01-21 01:04:47 +0800

    css fix

--- pages/reset_password.php
+++ pages/reset_password.php
@@ -4,24 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Reset Password</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<script>
-		function send_mail() {
-			alert("An E-mail has been sent to your enterprise mailbox. Check it to reset your password.");
-		}
-	</script>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="login.css" />
+    <script>
+        function send_mail() {
+            alert("An E-mail has been sent to your enterprise mailbox. Check it to reset your password.");
+        }
+    </script>
 </head>
+
 <body>
-    <div id="Reset_password">
-		<div id="name">Reset Password</div>
+    <div id="reset_password">
+        <div id="name">Reset Password</div>
         <form method="post">
-			<div>Please enter your enterprise E-mail address:</div>
+            <div>Please enter your enterprise E-mail address:</div>
             <input type="text" required="required" placeholder="E-mail address" name="e">
-            <button type="submit" onclick="send_mail()">Submit</button><!--Click here send an email to the employee's enterprise mailbox. A link will be in the email, click it to visit change_password.php-->
+            <button type="button" onclick="send_mail()" id="resetPassword">Send Email</button>
+            <!--Click here send an email to the employee's enterprise mailbox. A link will be in the email, click it to visit change_password.php-->
         </form>
     </div>
     <script>
-		
         if (false) { //if user information verified by server
             window.location.href = '../index.php';
         }
